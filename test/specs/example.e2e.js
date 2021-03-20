c7755dee5c7f9baca451d24f32728cc9876e7d1f
Alphy <qassurance@protonmail.com>
2021-03-19 21:21:53 -0700

    first test

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,14 +1,15 @@
 import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
+import ProfilePage from '../pageobjects/profile.page';
 
-describe('My Login application', () => {
-    it('should login with valid credentials', () => {
+describe('Auth', () => {
+    it('user logs in with valid data', () => {
         LoginPage.open();
+        LoginPage.setLogin('johnmons@test.com');
+        LoginPage.setPassword('331343');
+        LoginPage.clickSubmitButton();
+        ProfilePage.isOpen();
 
-        LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        expect(SecurePage.flashAlert).toBeExisting();
-        expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+
     });
 });
 
